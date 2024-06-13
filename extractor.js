16f21048988dee836cc993ebd5be00b8c68cc34e
PetarNK <89004582+PetarNK@users.noreply.github.com>
2024-06-13 17:40:29 +0300

    Add extractor functionality that will extract the data from the fetched file and return an array with the population data

---
+++ extractor.js
@@ -0,0 +1,23 @@
+// Getting the results from the document that was fetched by using Xpath in an
+export function getXPathResult(doc, path) {
+    const xpathResult = doc.evaluate(
+        path,
+        doc,
+        null,
+        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+        null
+    );
+    const result = [];
+    for (let i = 0; i < xpathResult.snapshotLength; i++) {
+        result.push(xpathResult.snapshotItem(i).textContent);
+    }
+    return result;
+}
+export function getFieldData(doc, name, fields) {
+    const fieldData = fields.flatMap((field) => {
+        const xPath = `//Item[@alias='${name}']/${field}`;
+        return getXPathResult(doc, xPath);
+    });
+    console.log(typeof fieldData);
+    return fieldData;
+}
